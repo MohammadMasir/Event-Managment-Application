f34c9faaf96ad09522b614dfd90b2157da94f872
LuckyTungariya <luckytungariya412@gmail.com>
2024-06-07 15:10:27 +0530

    delete3

--- data1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv('C:\\Users\\lucky\\OneDrive\\Documents\\house.csv')
-
-print(df[[df['id']==2])
--- employees.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-data={'name':['lucky','hansraj','dhruv','narayan','ghevar','dilip','sahil','jagruti','siddharth','aniket'],'company_name':['TCS','INFOSYS','HCL','HINDUSTHAN UNILIVER','CEAT','MRF','KOKABURA','BAS','NEW BALANCE','ENGLISH WILLOW'],'salary':[10000,20000,30000,40000,500000,600000,700000,800000,90000,100000]}
-df=pd.DataFrame(data)
-print(df)
-df.plot(kind='bar',x='name',y='salary',color='yellow')
-plt.title('scatterplot')
-plt.show()
-
