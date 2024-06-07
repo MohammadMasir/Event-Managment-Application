6b2c04459fe9e6726fa22791f333ba7f2ffdb8ef
LuckyTungariya <luckytungariya412@gmail.com>
2024-06-07 15:11:47 +0530

    delete4

--- dataframe.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-data = {'name':['lucky','hansraj','gautam','rohit'],'age':[24,34,45,56]}
-df = pd.DataFrame(data)
-print(df)
-df.plot(kind = 'hist', x = 'name', y = 'age', color = 'blue')
-plt.title('histogram')
-plt.show()
