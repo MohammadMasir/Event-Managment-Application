6d1a895b17201c8c0278bdccff63cf2961b2f40f
MohammadMasir <send2masir@gmail.com>
2024-06-10 18:44:28 -0700

    ` Integrated our own theme for the App using JSON. (File name : " my_custom_theme.json ") ` Added the Navigation from the Login page to the Main scren of the Application. ` Worked on Entering fullcreen & exiting the fullscreen using 'escape'. ` Refined the UI more better..

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import customtkinter as ctk
-from PIL import Image, ImageEnhance
+from PIL import Image
 import pymysql as pq
 from tkinter.messagebox import showinfo, showwarning, showerror
+import os
+
+#COLORS :  #093838 -> Primary Color.    } In the Theme,
+           #8bceba -> Secondary Color.  }  both the Primary & Secondary colors are used interchangeably.
+           #gainsboro -> Background Color.
+           #092928 -> Text Color.
+           #20807f -> Hover Bg Color.
+           #white -> Hover Text Color. 
 
 class DemoApplication(ctk.CTk):
     def __init__(self):
@@ -12,12 +20,18 @@ class DemoApplication(ctk.CTk):
         self.configure(fg_color="#093838")
 
         ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("green")
+        ctk.set_default_color_theme("my_custom_theme")
 
+        self.primary_color = "#093838"
+        self.secondary_color = "#8bceba"
+        self.bg = "gainsboro"
+        self.text_color = "#092928"
+        self.hovercolor_bg = "#20807f"
+        self.hovercolor_txt = "white"
         self.connection = self.connect_to_database()
         self.screen_stack = []
         self.current_screen = None
-        self.login_screen()
+        self.initial_screen()
 
     def connect_to_database(self):
         connection = pq.connect(
@@ -63,34 +77,79 @@ class DemoApplication(ctk.CTk):
             # Restore the previous screen from the stack
             self.current_screen = self.screen_stack.pop()
             self.current_screen()
+            
+    def back_but(self, 
+                 parent, 
+                 side="", 
+                 anchor=None, 
+                 padx=(0,0), pady=(0,0), 
+                 row=None, 
+                 column=None, 
+                 rowspan=None, 
+                 columnspan=None,
+                 ipady=None,
+                 ipadx=None,
+                 ):
+        back_img = ctk.CTkImage(dark_image=Image.open("back.png"))
 
-    def login_screen(self):
-        self.switch_screen(self._login_screen)
+        back_button = ctk.CTkButton(
+            parent,
+            text="",
+            font=("helvetica", 15),
+            command=self.back,
+            width=70, height=25,
+            corner_radius=100,
+            fg_color="#8bceba",
+            image=back_img,
+            text_color="#092928"
+        )
+        
+        if row is None:
+            back_button.pack(side=side, padx=padx, pady=pady, anchor=anchor)
+        else:
+            back_button.grid(row=row, column=column, padx=padx, pady=pady, columnspan=columnspan, rowspan=rowspan)
+        self.add_hover_effect(back_button)
 
-    def _login_screen(self):
+    def toggle_fullscreen(self):
+        # Set the application to fullscreen mode
+        self.attributes('-fullscreen', True)
+        self.bind("<Escape>", self.exit_fullscreen)
+
+    def exit_fullscreen(self, event=None):
+        width = self.winfo_screenwidth()
+        height = self.winfo_screenheight()
+        # Exit fullscreen mode
+        self.attributes('-fullscreen', False)
+        self.resizable(width=True, height=True)
+        self.geometry(f'{width - 200}x{height-100}')
+        self.maxsize(width,height)
+
+    def initial_screen(self):
+        self.switch_screen(self._initial_screen)
+
+    def _initial_screen(self):
         self.image_frame = ctk.CTkFrame(self)
-        self.image_frame.grid(row=0,column=0)
+        self.image_frame.grid(row=0, column=0)
 
         self.buttons_frame = ctk.CTkFrame(self, corner_radius=20, fg_color="gainsboro")
-        self.buttons_frame.grid(row=0,column=1, padx=20, pady=(0,10), ipady=70, ipadx=70)
+        self.buttons_frame.grid(row=0, column=1, padx=20, pady=(5, 10), ipady=70, ipadx=58)
 
-        self.bg_image = ctk.CTkImage(dark_image=Image.open("loginscreen_image.png"), size=(350,510))
+        self.bg_image = ctk.CTkImage(dark_image=Image.open("loginscreen_image.png"), size=(350, 510))
         self.image_label = ctk.CTkLabel(self.image_frame, text="", image=self.bg_image)
         self.image_label.pack()
         
         welcome_label = ctk.CTkLabel(
             self.buttons_frame, 
             text="Welcome \nTo \nEvent Manager",
-            font=('Franklin Gothic Medium Cond', 25, "bold"),
+            font=('Segoe UI', 25, "bold"),
             fg_color="#8bceba",
             compound="center",
-            height=70,
             text_color="white",
             corner_radius=9,
         ) 
         
         welcome_label.pack(
-            pady=(10,0), 
+            pady=(10, 0), 
             side="top", 
         )
 
@@ -116,7 +175,7 @@ class DemoApplication(ctk.CTk):
             text_color="#092928"
         )
 
-        login_button.pack(pady=(100,10), side="top")
+        login_button.pack(pady=(100, 10), side="top")
         register_button.pack(side="top")
 
         # Bind hover events for the buttons
@@ -128,39 +187,57 @@ class DemoApplication(ctk.CTk):
 
     def _register_screen(self, connection):
         self.image_frame = ctk.CTkFrame(self)
-        self.image_frame.grid(row=0,column=0)
+        self.image_frame.grid(row=0, column=0)
 
         self.buttons_frame = ctk.CTkFrame(self, corner_radius=20, fg_color="gainsboro")
-        self.buttons_frame.grid(row=0,column=1, padx=20, pady=(0,10))
+        self.buttons_frame.grid(row=0, column=1, padx=20, pady=(0, 10))
 
-        self.bg_image = ctk.CTkImage(dark_image=Image.open("loginscreen_image.png"), size=(350,510))
+        self.bg_image = ctk.CTkImage(dark_image=Image.open("loginscreen_image.png"), size=(350, 510))
         self.image_label = ctk.CTkLabel(self.image_frame, text="", image=self.bg_image)
         self.image_label.pack()
 
-        username_label = ctk.CTkLabel(self.buttons_frame, text="Enter username ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
+        welcome_label = ctk.CTkLabel(
+            self.buttons_frame, 
+            text="Register",
+            font=('Segoe UI.', 25, "bold"),
+            fg_color="#8bceba",
+            compound="center",
+            text_color="white",
+            corner_radius=9,
+        ) 
+        
+        welcome_label.grid(
+            pady=(10, 0), 
+            row=0, column=0,
+            columnspan=2,
+            ipady=6,
+            ipadx=10
+        )
+
+        email_label = ctk.CTkLabel(self.buttons_frame, text="Enter email ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
         password_label = ctk.CTkLabel(self.buttons_frame, text="Enter password ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
         retype_password_label = ctk.CTkLabel(self.buttons_frame, text="Retype password ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
 
-        username_label.grid(row=0, column=0, sticky="nsew", pady=(90,10), padx=(20,10))
-        password_label.grid(row=1, column=0, sticky="nsew", padx=(20,10))
-        retype_password_label.grid(row=2, column=0, sticky="nsew", pady=10, padx=(20,10))
+        email_label.grid(row=1, column=0, sticky="w", pady=(50, 10), padx=(20, 10))
+        password_label.grid(row=2, column=0, sticky="w", padx=(20, 10))
+        retype_password_label.grid(row=3, column=0, sticky="w", pady=10, padx=(20, 10))
 
-        username = ctk.StringVar()
+        email = ctk.StringVar()
         password = ctk.StringVar()
         retype_password = ctk.StringVar()
 
-        username_field = ctk.CTkEntry(self.buttons_frame, textvariable=username)
+        email_field = ctk.CTkEntry(self.buttons_frame, textvariable=email)
         password_field = ctk.CTkEntry(self.buttons_frame, textvariable=password, show='*')
         retype_pass_field = ctk.CTkEntry(self.buttons_frame, textvariable=retype_password, show='*')
 
-        username_field.grid(row=0, column=1, sticky="nsew", pady=(90,10), padx=(0,10))
-        password_field.grid(row=1, column=1, sticky="nsew", padx=(0,10))
-        retype_pass_field.grid(row=2, column=1, sticky="nsew", pady=10, padx=(0,10))
+        email_field.grid(row=1, column=1, sticky="nsew", pady=(50, 10), padx=(0, 8))
+        password_field.grid(row=2, column=1, sticky="nsew", padx=(0, 8))
+        retype_pass_field.grid(row=3, column=1, sticky="nsew", pady=10, padx=(0, 8))
 
         def check():
-            if username.get().isdigit() or password.get().isdigit():
+            if email.get().isdigit() or password.get().isdigit():
                 showerror("Value Error!", "Please input Characters.")
-            elif username.get() == "" or password.get() == "":
+            elif email.get() == "" or password.get() == "":
                 showerror("Value Error!", "Please input Characters.")
             else:
                 if password.get() != retype_password.get():
@@ -168,18 +245,18 @@ class DemoApplication(ctk.CTk):
                 else:
                     with connection.cursor() as cursor:
                         sql = "SELECT * FROM user WHERE user_name = %s"
-                        cursor.execute(sql, (username.get(),))
+                        cursor.execute(sql, (email.get(),))
                         result = cursor.fetchone()
 
                     if result:
-                        showinfo("Registration failed.", "Username already exists.")
+                        showinfo("Registration failed.", "email already exists.")
                     else:
                         with connection.cursor() as cursor:
                             sql = "INSERT INTO user (user_name, password) VALUES (%s, %s)"
-                            cursor.execute(sql, (username.get(), password.get()))
+                            cursor.execute(sql, (email.get(), password.get()))
                             connection.commit()
-                            showinfo("Done!", "Registration successful!")
-                            self.create_widgets()
+                            showinfo("Done!", "Registration successful!\nNow you can Login.")
+                            self.login(self.connection)
 
         check_button = ctk.CTkButton(
             self.buttons_frame, 
@@ -191,26 +268,11 @@ class DemoApplication(ctk.CTk):
             fg_color="#8bceba",
             text_color="#092928",
         )
-        check_button.grid(row=3, column=0, columnspan=2, padx=(40,20), pady=(17,0))
+        check_button.grid(row=4, column=0, columnspan=2, padx=(40, 20), pady=(17, 0))
 
         self.add_hover_effect(check_button)
 
-        back_img = ctk.CTkImage(dark_image=Image.open("back.png"))
-
-        back_button = ctk.CTkButton(
-            self.buttons_frame,
-            text="",
-            font=("helvetica", 15),
-            command=self.back,
-            width=70, height=25,
-            corner_radius=100,
-            fg_color="#8bceba",
-            image=back_img,
-            text_color="#092928"
-        )
-        back_button.grid(row=4, column=0, columnspan=2, padx=(0, 200), pady=(90,10))
-
-        self.add_hover_effect(back_button)
+        self.back_but(self.buttons_frame, row=5, column=0, columnspan=2, padx=(0, 200), pady=(90, 10))
 
     def login(self, connection):
         self.switch_screen(lambda: self._login(connection))
@@ -220,36 +282,54 @@ class DemoApplication(ctk.CTk):
         self.image_frame.grid(row=0,column=0)
 
         self.buttons_frame = ctk.CTkFrame(self, corner_radius=20, fg_color="gainsboro")
-        self.buttons_frame.grid(row=0,column=1, padx=20, pady=(0,10))
+        self.buttons_frame.grid(row=0,column=1, padx=20, pady=(0,10),ipadx=3)
 
         self.bg_image = ctk.CTkImage(dark_image=Image.open("loginscreen_image.png"), size=(350,510))
         self.image_label = ctk.CTkLabel(self.image_frame, text="", image=self.bg_image)
         self.image_label.pack()
 
-        username_label = ctk.CTkLabel(self.buttons_frame, text="Enter username: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
-        password_label = ctk.CTkLabel(self.buttons_frame, text="Enter password: ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
+        welcome_label = ctk.CTkLabel(
+            self.buttons_frame, 
+            text="Login",
+            font=('Segoe UI.', 25, "bold"),
+            fg_color="#8bceba",
+            compound="center",
+            text_color="white",
+            corner_radius=9,
+        ) 
+        
+        welcome_label.grid(
+            pady=(10, 0), 
+            row=0, column=0,
+            columnspan=2,
+            ipady=8,
+            ipadx=8
+        )
 
-        username_label.grid(row=0, column=0, sticky="nsew", pady=(90,10), padx=(20,10))
-        password_label.grid(row=1, column=0, sticky="nsew", padx=(20,10))
+        email_label = ctk.CTkLabel(self.buttons_frame, text="Enter email ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
+        password_label = ctk.CTkLabel(self.buttons_frame, text="Enter password ", font=ctk.CTkFont(size=15, weight="bold"), text_color="#092928")
 
-        username = ctk.StringVar()
+        email_label.grid(row=1, column=0, sticky="w", pady=(50,10), padx=(20,10))
+        password_label.grid(row=2, column=0, sticky="nsew", padx=(20,10))
+
+        email = ctk.StringVar()
         password = ctk.StringVar()
 
-        username_field = ctk.CTkEntry(self.buttons_frame, textvariable=username)
+        email_field = ctk.CTkEntry(self.buttons_frame, textvariable=email)
         password_field = ctk.CTkEntry(self.buttons_frame, textvariable=password, show='*')
 
-        username_field.grid(row=0, column=1, pady=(90,10), padx=(0,10))
-        password_field.grid(row=1, column=1, padx=(0,10))
+        email_field.grid(row=1, column=1, pady=(50,10), padx=(0,10))
+        password_field.grid(row=2, column=1, padx=(0,10))
 
         def check():
-            if username.get().isdigit() or password.get().isdigit():
+            if email.get().isdigit() or password.get().isdigit():
                 showerror("Value Error!", "Please input Characters.")
-            elif username.get() == "" or password.get() == "":
+            elif email.get() == "" or password.get() == "":
                 showerror("Value Error!", "Please input Characters.")
             else:
                 cursor = connection.cursor()
                 sql = "SELECT * FROM user WHERE user_name = %s AND password = %s"
-                cursor.execute(sql, (username.get(), password.get()))
+                cursor.execute(sql, (email.get(), password.get()))
                 result = cursor.fetchone()
 
                 if result:
@@ -265,69 +345,71 @@ class DemoApplication(ctk.CTk):
             text="Submit", 
             command=check,
             font=("helvetica", 20),
-            width=85, height=35,
+            width=25, height=35,
             corner_radius=100,
             fg_color="#8bceba",
             text_color="#092928",
             )
-        check_button.grid(row=2, column=0, columnspan=2, padx=(40,20), pady=(17,0))
-
-        back_img = ctk.CTkImage(dark_image=Image.open("back.png"))
-
-        back_button = ctk.CTkButton(
-            self.buttons_frame,
-            text="",
-            font=("helvetica", 15),
-            command=self.back,
-            width=70, height=25,
-            corner_radius=100,
-            fg_color="#8bceba",
-            image=back_img,
-            text_color="#092928"
-        )
-        back_button.grid(row=3, column=0, columnspan=2, padx=(0, 200), pady=(90,10))
+        check_button.grid(row=3, column=0, columnspan=2, padx=(40,20), pady=(17,0))
 
         self.add_hover_effect(check_button)
-        self.add_hover_effect(back_button)
+        self.back_but(self.buttons_frame,row=4, column=0, columnspan=2, padx=(0, 200), pady=(70,10))
         
     def create_widgets(self):
+        self.switch_screen(self._create_widgets)
+
+    def _create_widgets(self):
+        self.toggle_fullscreen()
+
         for widget in self.winfo_children():
             widget.destroy()
 
-        frame2 = ctk.CTkFrame(self, height=30, fg_color="#2fa572")
-        frame2.pack(fill="both")
+        frame2 = ctk.CTkFrame(self, height=30, fg_color=self.bg)
+        frame2.pack(fill="x")
 
-        labe = ctk.CTkLabel(frame2, text="Event Management System", font=("Segoe UI", 40, "bold"))
-        labe.grid(row=0, column=0, padx=(420, 220))
-
-        self.head_img = ctk.CTkImage(dark_image=Image.open("Logo.png"), size=(200, 90))
-        head_label = ctk.CTkLabel(frame2, image=self.head_img, text="")
-        head_label.grid(row=0, column=1)
+        labe = ctk.CTkLabel(frame2, text="Event Manager", font=("Segoe UI", 40, "bold"), text_color="white", fg_color=self.secondary_color, corner_radius=12)
+        labe.pack(pady=10, ipady=5,expand=True)
 
         # Create a notebook (tabbed interface)
-        notebook = ctk.CTkTabview(self, width=700, height=500, fg_color="#3fa572")
+        notebook = ctk.CTkTabview(self, width=700, height=500, bg_color=self.bg, fg_color=self.bg, corner_radius=12) #3fa572 
         notebook.pack(padx=20, pady=20, fill="both", expand=True)
+        # notebook.set(self.event_tab)
 
         # Dashboard Tab
-        dashboard_tab = notebook.add("Dashboard")
+        self.dashboard_tab = notebook.add("Dashboard")
+        self.dashboard_widgets()
 
         # Event Management Tab
-        event_tab = notebook.add("Event Management")
-        ctk.CTkLabel(event_tab, text="Manage your events here", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        self.event_tab = notebook.add("Event Management")
+        self.eventtab_widgets()
 
         # Registration and Ticketing Tab
-        ticket_tab = notebook.add("Registration and Ticketing")
-        ctk.CTkLabel(ticket_tab, text="Handle registrations and tickets", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        self.ticket_tab = notebook.add("Registration and Ticketing")
+        self.tickettab_widgets()
 
         # Reporting and Analytics Tab
-        report_tab = notebook.add("Reporting and Analytics")
-        ctk.CTkLabel(report_tab, text="View analytics and reports", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        self.report_tab = notebook.add("Reporting and Analytics")
+        self.reporttab_widgets()
 
         # Survey and Feedback Tab
-        survey_tab = notebook.add("Survey and Feedback")
-        ctk.CTkLabel(survey_tab, text="Create surveys and view responses", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        self.survey_tab = notebook.add("Survey and Feedback")
+        self.surveytab_widgets()
 
+    def dashboard_widgets(self):
+        ctk.CTkLabel(self.dashboard_tab, text="Welcome to Your Dashboard!", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+
+    def eventtab_widgets(self):
+        ctk.CTkLabel(self.event_tab, text="Manage your events here", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+
+    def tickettab_widgets(self):
+        ctk.CTkLabel(self.ticket_tab, text="Handle registrations and tickets", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+
+    def reporttab_widgets(self):
+        ctk.CTkLabel(self.report_tab, text="View analytics and reports", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+
+    def surveytab_widgets(self):
+        ctk.CTkLabel(self.survey_tab, text="Create surveys and view responses", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        
 if __name__ == "__main__":
     app = DemoApplication()
     app.mainloop()
-
